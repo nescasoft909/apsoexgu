05de6c96f51c1d089f53c438b186c8c0a9e2d0e6
Manuel Vera <manuel.vera@aplisoft.com.ec>
2014-10-23 12:30:21 -0500

    primera presentacion
    primera presentacion, calendario fotos, menu y form

--- www/js/index.js
+++ www/js/index.js
@@ -48,19 +48,64 @@ var app = {
     }
 };
 
-//funciones
-function go_to(direccion){
-    
-    if(direccion =="login"){
-            /*$.ajax({
-                type: "POST",
-                url: 'login.html',
-                //data: formData,
-                success: function (data) {
-                    $("#content-page").html(data);
-                }
-            });*/
-        $.mobile.changePage( "login.html", { transition: "flip" });
-    }
+function cambiarPagina(url){
+    location.href=url;
+    //$.mobile.changePage( url, { transition: "slide" });
 }
+
+function loadMenu(){
+    $('#menuToggle').click(function(){
+                var $parent = $(this).parent('nav');
+                $parent.toggleClass("open");
+                var navState = $parent.hasClass('open') ? "hide" : "show";
+                $(this).attr("title", navState + " navigation");
+                // Set the timeout to the animation length in the CSS.
+                setTimeout(function(){
+                console.log("timeout set");
+                $('#menuToggle > span').toggleClass("navClosed").toggleClass("navOpen");
+                }, 200);
+                //e.preventDefault();
+            });
+}
+
+
+/*var app ={
+
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('menu');
+        
+        $('#menuToggle').click(function(){
+            var $parent = $(this).parent('nav');
+            $parent.toggleClass("open");
+            var navState = $parent.hasClass('open') ? "hide" : "show";
+            $(this).attr("title", navState + " navigation");
+            // Set the timeout to the animation length in the CSS.
+            /*setTimeout(function(){
+            console.log("timeout set");
+            $('#menuToggle > span').toggleClass("navClosed").toggleClass("navOpen");
+            }, 200);
+            //e.preventDefault();
+        });    
+    }
+};*/
+
 
